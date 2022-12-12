1ec491ba45f3077149d272565561469b2a00c66f
kim deokkeun <deokk91@gmail.com>
2022-12-12 15:16:02 +0900

    array first project

---
+++ src/edu/kh/array/ex/ArrayRun.java
@@ -0,0 +1,18 @@
+package edu.kh.array.ex;
+
+public class ArrayRun {
+
+	public static void main(String[] args) {
+		
+		ArrayExample1 arrEx = new ArrayExample1();
+		
+		//arrEx.ex1();
+		//arrEx.ex2();
+		//arrEx.ex3();
+		//arrEx.ex4();
+		//arrEx.ex5();
+		//arrEx.ex6();
+		arrEx.ex8();
+	}
+
+}
